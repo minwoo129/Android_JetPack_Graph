15e3b06497c318f9e808fd0f088c933722e44b7f
minwoo129 <key0129mw@naver.com>
2023-09-09 10:33:02 +0900

    원형 그래프 페이지 그리기

--- app/src/main/java/com/example/graph/screen/Graph1.kt
+++ app/src/main/java/com/example/graph/screen/Graph1.kt
@@ -1,17 +1,77 @@
 package com.example.graph.screen
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.graph.ui.theme.GraphTheme
 
 @Composable
 fun Graph1() {
-    Text(
-        text = "1",
-        fontSize = 30.sp
-    )
+    Box(
+        modifier = Modifier.padding(top = 120.dp)
+    ) {
+        Graph1Gauge(percent = 70f, maxPercent = 100f)
+    }
+}
+
+@Composable
+fun Graph1Gauge(percent: Float, maxPercent: Float) {
+    val colorPercent = 360 * (percent/maxPercent)
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+            .padding(20.dp)
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            drawArc(
+                color = Color.LightGray,
+                startAngle = 0f,
+                sweepAngle = 360f,
+                useCenter = false,
+                style = Stroke(32f)
+            )
+            drawArc(
+                color = Color.Blue,
+                startAngle = -90f,
+                sweepAngle = colorPercent,
+                useCenter = false,
+                style = Stroke(32f)
+            )
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "${((percent / maxPercent) * 100).toInt()}%",
+                fontSize = 35.sp
+            )
+            Text(
+                text = "$percent / $maxPercent",
+                fontSize = 20.sp
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
