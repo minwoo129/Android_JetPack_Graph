96e3ddec905167cd4720a00bf37e77830d171cdf
minwoo129 <key0129mw@naver.com>
2023-09-09 10:05:37 +0900

    네비게이션 바 생성 및 연결

--- app/src/main/java/com/example/graph/MainActivity.kt
+++ app/src/main/java/com/example/graph/MainActivity.kt
@@ -3,12 +3,26 @@ package com.example.graph
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.graph.ui.theme.GraphTheme
 
@@ -17,30 +31,64 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GraphTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                GraphAppMain()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun GraphAppMain() {
+    val navItems = listOf(
+        NavigationItem("그래프1", Icons.Default.AddCircle, "tab1"),
+        NavigationItem("그래프2", Icons.Default.AddCircle, "tab2"),
+        NavigationItem("그래프3", Icons.Default.AddCircle, "tab3"),
     )
+    
+    Scaffold(
+        bottomBar = {
+            BottomAppBar {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    navItems.forEach {nav ->
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Icon(
+                                imageVector = nav.icon,
+                                contentDescription = nav.name
+                            )
+                            Text(
+                                text = nav.name
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    ) { paddingValues -> 
+        Box(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            
+        }
+    }
 }
+
+data class NavigationItem(
+    val name: String,
+    val icon: ImageVector,
+    val route: String
+)
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GraphTheme {
-        Greeting("Android")
+        GraphAppMain()
     }
 }
