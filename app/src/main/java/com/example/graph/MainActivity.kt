1bedb4627aceedf5a3dbf3b9b18f51b55bd21a3f
minwoo129 <key0129mw@naver.com>
2023-09-09 10:19:58 +0900

    네비게이션 배치 구성

--- app/src/main/java/com/example/graph/MainActivity.kt
+++ app/src/main/java/com/example/graph/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.graph
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,8 +23,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.graph.screen.Graph1
+import com.example.graph.screen.Graph2
+import com.example.graph.screen.Graph3
 import com.example.graph.ui.theme.GraphTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,6 +49,9 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GraphAppMain() {
+
+    val navController = rememberNavController()
+
     val navItems = listOf(
         NavigationItem("그래프1", Icons.Default.AddCircle, "tab1"),
         NavigationItem("그래프2", Icons.Default.AddCircle, "tab2"),
@@ -54,28 +66,54 @@ fun GraphAppMain() {
                     horizontalArrangement = Arrangement.SpaceAround
                 ) {
                     navItems.forEach {nav ->
+                        val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route;
+
                         Column(
                             verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.clickable {
+                                navController.navigate(nav.route)
+                            }
                         ) {
+                            val isCurrentRoute = nav.route == currentRoute
+
+
                             Icon(
                                 imageVector = nav.icon,
-                                contentDescription = nav.name
+                                contentDescription = nav.name,
+                                tint = if(isCurrentRoute) Color.Red else Color.Black
                             )
                             Text(
-                                text = nav.name
+                                text = nav.name,
+                                color = if(isCurrentRoute) Color.Red else Color.Black
                             )
                         }
                     }
                 }
             }
         }
-    ) { paddingValues -> 
-        Box(
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = navItems.first().route,
             modifier = Modifier.padding(paddingValues)
         ) {
-            
+            composable("tab1") {
+                Graph1()
+            }
+            composable("tab2") {
+                Graph2()
+            }
+            composable("tab3") {
+                Graph3()
+            }
+
         }
+//        Box(
+//            modifier = Modifier.padding(paddingValues)
+//        ) {
+//            Text(text = "임시 텍스트")
+//        }
     }
 }
 
